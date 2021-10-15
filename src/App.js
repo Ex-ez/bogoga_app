12861df4e8fb4cad6931fa8230f81edda36648ac
Ex-ez <khkkor3@naver.com>
2021-10-15 14:06:31 +0900

    Poster

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from "react";
-import Potato from "./Potato";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function App() {
-    return <div>
-      <h1>Hello</h1>
-      <Potato />
-    </div>;
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+             <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+           )}
+      </section>
+    );
+  }
 }
 
 export default App;
